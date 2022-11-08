9f2b3caa8a79ac5f1d282e4cd11566933b29e0e3
fernando <fernando.silva927@etec.sp.gov.br>
2022-11-07 22:23:09 -0300

    projeto inicial

---
+++ CURRICULO3.1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CURRICULO3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome;
+            String sobrenome;
+            String data_nascimento;
+            String endereço;
+            String sexo;
+            String escolaridade;
+            String cursos_extra_curriculares;
+            String experiencia_profissional;
+            String email;
+            String telefone;
+
+            Console.WriteLine("nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("sobrenome");
+            sobrenome = Console.ReadLine();
+            Console.WriteLine(nome + " " + sobrenome);
+            Console.WriteLine("email");
+            email = Console.ReadLine();
+            Console.WriteLine("telefone");
+            telefone = Console.ReadLine();
+            Console.WriteLine("data de nascimento");
+            data_nascimento = Console.ReadLine();
+            Console.WriteLine("endereço");
+            endereço = Console.ReadLine();
+            Console.WriteLine("sexo");
+            sexo = Console.ReadLine();
+            Console.WriteLine("escolaridade");
+            escolaridade = Console.ReadLine();
+            Console.WriteLine("cursos extra-curriculares");
+            cursos_extra_curriculares = Console.ReadLine();
+            Console.WriteLine("experiencia profissional");
+            experiencia_profissional = Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
